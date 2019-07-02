b003cbdaf9db730dd4ccacaaafa1b5c9601f2644
k-masashi <masan580@gmail.com>
2019-07-02 10:34:53 +0900

    Initial Commit

---
+++ RootVCTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RootVCTest
+//
+//  Created by Kunugi Masashi on 2019/07/02.
+//  Copyright © 2019 Globis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
